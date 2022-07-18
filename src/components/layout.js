232041d6087af8f43658c9389b3a63db3619e018
snowyuki <y-maeda.1031@keio.jp>
2022-07-19 00:01:17 +0900

    Add blog function

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,25 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { container, heading } from "./layout.module.css"
+import { useStaticQuery, graphql } from "gatsby"
+import { container, siteTitle } from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(
+    graphql`
+    query{
+      site {
+        siteMetadata {
+          title
+        }
+      }  
+    }`
+  )
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title} </title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav></nav>
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
