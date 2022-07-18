4015478fce0d815f9c3189d3c6ac670fedd13bdc
snowyuki31 <snowyuki31@users.noreply.github.com>
2022-07-18 13:25:15 +0000

    deploy: c25c5b05457953932da602058f20bd1c6e10c88c

---
+++ component---src-pages-index-js-8a29550d83189c486ee3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[678],{8110:function(n,t,e){e.r(t),e.d(t,{default:function(){return a}});e(7294),e(1597);var l=e(7946),u=function(n){var t=n.pageTitle,e=n.children;return(0,l.tZ)("div",{className:"layout-module--container--eLBMS"},(0,l.tZ)("title",null,t),(0,l.tZ)("nav",null),(0,l.tZ)("main",null,(0,l.tZ)("h1",{className:"layout-module--heading--8VjLO"},t),e))},a=function(){return(0,l.tZ)(u,{pageTitle:"snowyuki"})}}}]);
+//# sourceMappingURL=component---src-pages-index-js-8a29550d83189c486ee3.js.map
